4e42c2f61c61268341797d7d305dce581bd0aeff
Brenda Thayillam <bsthayillam@gmail.com>
2017-04-13 18:42:28 -0400

    Sequential tester code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cassert>
 
 #include "seq_hash_table.h"
 
-#define INPUT_SIZE 2000
+enum Instr {
+  insert,
+  del,
+  lookup
+};
 
 int hash(int tag) {
   int temp = tag;
@@ -15,20 +23,75 @@ int hash(int tag) {
   return abs(hashVal);
 }
 
-int main() {
-    SeqHashTable<int,int> table(INPUT_SIZE/2, &hash);
-    std::vector<int> keys;
-    for (int i = 0; i < INPUT_SIZE; i++) {
-        int key = rand();
-        int val = rand();
-        keys.push_back(key);
-        table.insert(key, val);
-    }
+std::vector<std::pair<Instr, std::pair<int, int> > > parseText(const std::string &filename)
+{
+  std::ifstream infile;
+  infile.open(filename.c_str());
+  std::vector<std::pair<Instr, std::pair<int, int> > > instructions;
+  while(!infile.eof())
+  {
+    std::string str;
+    std::pair<Instr, std::pair<int, int> > task;
+    getline(infile, str);
+    if (str.length() > 0)
+    {
+      char instr = str[0];
+      switch(instr) {
+        case 'L':
+          task.first = lookup;
+          break;
+        case 'I':
+          task.first = insert;
+          break;
+        case 'D':
+          task.first = del;
+          break;
+        default:
+          break;
+        }
 
-    for (int i = 0; i < INPUT_SIZE; i++) {
-        int keyIndex = rand() % keys.size();
-        int key = keys.back();
-        keys.pop_back();
-        table.remove(key);
+      std::pair<int, int> keyval;
+      std::string rest = str.substr(2);
+      int spcIdx = rest.find_first_of(' ');
+      keyval.first = atoi(rest.substr(0, spcIdx).c_str());
+      keyval.second = atoi(rest.substr(spcIdx + 1).c_str());
+      task.second = keyval;
+      instructions.push_back(task);
     }
+  }
+  return instructions;
+}
+
+void seqTest(std::vector<std::pair<Instr, std::pair<int, int> > > input)
+{
+  SeqHashTable<int,int> table(input.size() / 2, &hash);
+  for (int i = 0; i < input.size(); i++)
+  {
+    std::pair<Instr, std::pair<int, int> > instr = input[i];
+    switch(instr.first)
+    {
+      case insert:
+        table.insert(instr.second.first, instr.second.second);
+        break;
+      case del:
+        assert(table.remove(instr.second.first)->get_data() == instr.second.second);
+        break;
+      case lookup:
+        assert(table.find(instr.second.first)->get_data() == instr.second.second);
+        break;
+      default:
+        break;
+    }
+  }
+  printf("Test Complete! (No Errors Detected)\n");
+}
+
+int main() {
+    std::vector<std::pair<Instr, std::pair<int, int> > > input = parseText("tests/uniform_all_test.txt");
+    seqTest(input);
+    // coarseLockTest(input);
+    // fineLockTest(input);
+    // insertOptimalTest(input);
+    // deleteOptimalTest(input);
+    // dynamicTest(input);
 }
