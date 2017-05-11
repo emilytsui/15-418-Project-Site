8f5c8f6328babe54410c8f421167415341d26c0a
emilytsui <emilytsui7@gmail.com>
2017-05-11 17:42:01 -0400

    Bigger load factors

--- src/loadFactorTest.cpp
+++ src/loadFactorTest.cpp
@@ -205,7 +205,8 @@ int main() {
         ids[z] = z;
     }
     double baseTime;
-    for (int load_fac = 1; load_fac <= 20; load_fac = load_fac == 1 ? 5 : load_fac + 5) {
+    for (int load_fac = 250; load_fac <= 1000; load_fac *= 2) {
+    // for (int load_fac = 1; load_fac <= 20; load_fac = load_fac == 1 ? 5 : load_fac + 5) {
         printf("\nPerformance Testing file with load factor %d: %s on fine-grained lock-based hash table\n", load_fac, filename.c_str());
         parseText(filename.c_str());
         baseline = new SeqHashTable<int, int>(200000/load_fac, &hash);
